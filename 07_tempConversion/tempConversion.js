0eecaace9ec5c05e7eb913aeeea2d0f893bbff42
Brad Mintun <bradltl@gmail.com>
2023-01-19 16:17:24 -0600

    finished through 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  
+  //if (!Number.isInteger(temp)) return "ERROR";
+  let returnTemp = 0;
+  // Deduct 32, then multiply by 5, then divide by 9
+  temp = (((temp - 32) *5) / 9);
+  returnTemp = Math.round(temp * 10) / 10
+  return returnTemp;
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  // °F = °C × (9/5) + 32
+  //if (!Number.isInteger(temp)) return "ERROR";
+  let returnTemp = 0;
+  temp = (((temp * 9) /5) + 32);
+  temp = (Math.round(temp + "e+" + 1)  + "e-" + 1);
+  returnTemp = Math.round(temp * 10) / 10
+  return returnTemp;
+
+
 };
 
 // Do not edit below this line
