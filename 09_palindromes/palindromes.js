b665cadea3b57b8ddcdffdb815c7247c8c4b0d83
Brad Mintun <bradltl@gmail.com>
2023-01-20 09:42:03 -0600

    complete through 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,32 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+
+    //Use Regex to remove non-character values from string
+
+    let origString = inputString.replace(/[^a-zA-Z]+/g, '');
+
+
+    let reverseString = '';
+
+    //Reverse the string characters
+    for (let i = inputString.length; i >= 0; i--) {
+        reverseString = reverseString + inputString.charAt(i);
+
+    }
+
+    //Remove non-character values
+    reverseString = reverseString.replace(/[^a-zA-Z]+/g, '');
+
+    //Convert both strings to lower case for comparison
+    origString = origString.toLowerCase();
+    reverseString = reverseString.toLowerCase();
+
+
+    //Check to see if the strings match
+    if (origString === reverseString) {
+        return true;
+    }
+
+    return false;
 
 };
 
