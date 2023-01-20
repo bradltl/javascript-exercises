a52561b04b43143fc9194ae6785a94e8af02a2a6
Brad Mintun <bradltl@gmail.com>
2023-01-19 21:24:06 -0600

    completed through 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,58 @@
-const add = function() {
+const add = function(first, second) {
+  let answer = 0;
+  answer = first + second;
+  return answer;
 	
 };
 
-const subtract = function() {
+const subtract = function(first, second) {
+  let answer = 0;
+  answer = first - second;
+  return answer;
 	
 };
 
-const sum = function() {
+const sum = function(sumValues) {
+	let answer = 0;
+
+  for (let i = 0; i < sumValues.length; i++) {
+    answer = answer + sumValues[i];
+  }
+  return answer;
+
+};
+
+const multiply = function(multiValues) {
+  let answer = 1;
+
+  for (let i = 0; i < multiValues.length; i++) {
+    answer = answer * multiValues[i];
+  }
+
+  return answer;
+
+};
+
+const power = function(first, second) {
+  let answer = 0;
+  answer = first ** second;
+  return answer;
 	
 };
 
-const multiply = function() {
+const factorial = function(factValue) {
+  let answer = 1;
 
-};
+  if (factValue === 0) {
+    answer = 1;
+    return answer;
+  }
 
-const power = function() {
-	
-};
+  for (let i = 1; i <= factValue; i++) {
+    answer = answer * i;
+  }
 
-const factorial = function() {
+  return answer;
 	
 };
 
